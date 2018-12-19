a86058d59232796b16f43df3a65cd71c0331d390
shemalatha917 <45626568+shemalatha917@users.noreply.github.com>
2018-12-19 14:33:01 +0530

    Update HelloWorld.java
    New commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,4 +4,14 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+    int user = 21;
+if (user <=18){
+System.out.println("user is 18 or younge)
+}
+else if (user >18 && user < 40){
+System.out.println("user is between 19 years")
+}
+else{
+System.out.println("user is older than 40")}
+}
 }
